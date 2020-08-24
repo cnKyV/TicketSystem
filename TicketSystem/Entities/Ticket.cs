8adc1d9d70d334956355576253df510a2425b341
Cenkay Vergili <31133723+cnKyV@users.noreply.github.com>
2020-08-24 15:54:09 +0300

    Add project files.

---
+++ TicketSystem/Entities/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketSystem.Entities
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
